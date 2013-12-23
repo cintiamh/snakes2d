9462f5b44c665e17abb39e0076c53b14f4e2593f
Cintia Higashi <cintiamh@gmail.com>
2013-12-22 22:27:54 -0800

    Script main structure and animation loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,48 @@
+var SNAKES = {};
 
-// block
+// Block
+SNAKES.Block = function() {
 
-// snake
+}
+
+// Grid
+SNAKES.Grid = function() {
+
+}
 
 // apple
+SNAKES.Apple = function() {
+
+}
+
+// Snake's body section
+SNAKES.Body = function() {
+
+}
+
+// snake
+SNAKES.Snake = function() {
+
+}
 
 // game
+SNAKES.Game = function() {
+
+}
+
 
 // main function - animation loop
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+(function animloop(){
+    requestAnimFrame(animloop);
+//    render();
+})();
